015d4d150a0c26535df292849f445afe765dbe98
B4lex <b4lexxx@gmail.com>
2021-03-01 19:44:41 +0200

    Remove unnecessary app.

--- src/server/settings.py
+++ src/server/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    'rest_framework',
-    'accounts',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
