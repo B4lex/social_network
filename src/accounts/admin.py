f8c1d7b0a667efebed3d8263e4df96c781820d15
B4lex <b4lexxx@gmail.com>
2021-03-01 19:40:52 +0200

    Create People tab functionality. Implement user's search and follow system.

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from accounts.models import Profile
+from accounts.models import ExtendedUser, FollowRelation
 
 
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user',)
+@admin.register(ExtendedUser)
+class ExtendedUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
 
+
+@admin.register(FollowRelation)
+class FollowRelationAdmin(admin.ModelAdmin):
+    list_display = ('follower', 'follows_to', 'followed_at')
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,29 +1,33 @@
 from django.shortcuts import redirect
+from django.http import JsonResponse
 from django.contrib.auth.views import LoginView
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
 from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
+from django.db.models import Q
 
 from accounts import forms
+from accounts.models import FollowRelation
 
 
 class UserLoginView(LoginView):
-    template_name = 'login.html'
+    template_name = 'auth/login.html'
     redirect_authenticated_user = True
 
 
 class UserRegisterView(CreateView):
-    template_name = 'register.html'
+    template_name = 'auth/register.html'
     form_class = forms.UserRegisterForm
     success_url = reverse_lazy('accounts:dashboard')
 
     def form_valid(self, form):
         return_value = super().form_valid(form)
         self.object.is_staff = True
-        self.object.profile.create()
         login(self.request, self.object)
         return return_value
 
@@ -39,28 +43,56 @@ class UserEditView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     template_name = 'profile_edit.html'
     success_url = reverse_lazy('accounts:dashboard')
     success_message = 'Changes successfully saved.'
-    profile_form = forms.ProfileEditForm
 
     def get_object(self, queryset=None):
         return self.request.user
 
-    def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
-        context_data['profile_form'] = self.profile_form(instance=self.object.profile)
-        return context_data
-
-    def post(self, request, *args, **kwargs):
-        profile_form = self.profile_form(
-            instance=request.user.profile,
-            data=request.POST,
-            files=request.FILES
-        )
-        if profile_form.is_valid():
-            profile_form.save()
-            return super().post(request, *args, **kwargs)
-        else:
-            return super().form_invalid(self.get_form())
-
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard.html'
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    template_name = 'person_detail.html'
+    context_object_name = 'person'
+
+
+class UserListView(LoginRequiredMixin, ListView):
+    template_name = 'people_list.html'
+    context_object_name = 'people'
+    model = get_user_model()
+    queryset = model.objects.prefetch_related().all()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('query')
+        if query:
+            queryset = queryset.filter(
+                Q(username__icontains=query) | Q(first_name__icontains=query) |
+                Q(last_name__icontains=query) | Q(email__icontains=query)
+            )
+        return queryset
+
+
+def follow_person(request):
+    follow_to_id = request.POST.get('user_id')
+    action = request.POST.get('action')
+    failed = False
+
+    user = get_user_model().objects.get(id=follow_to_id)
+    if request.user != user:
+        if action == 'follow' and user not in request.user.following.all():
+            FollowRelation.objects.create(follower=request.user, follows_to=user)
+        elif action == 'unfollow':
+            try:
+                FollowRelation.objects.get(follower=request.user, follows_to=user).delete()
+            except FollowRelation.DoesNotExist:
+                pass
+        else:
+            failed = True
+    else:
+        failed = True
+    return JsonResponse({'status': 'ok' if not failed else 'fail'})
